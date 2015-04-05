d00a8dc573939637e6908622ea914a343105900f
Neha <neha.sahota@gmail.com>
2015-04-05 19:08:24 -0400

    added registration servlet

--- src/java/servlet/RegistrationServlet.java
+++ src/java/servlet/RegistrationServlet.java
@@ -6,8 +6,11 @@
 
 package servlet;
 
+import beans.Student;
+import static com.sun.xml.ws.api.message.Packet.Status.Request;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -29,22 +32,34 @@ public class RegistrationServlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException 
+    {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+       PrintWriter out = response.getWriter();
+       try
+       {
+           Student student = new Student();
+           
+           student.setFirstName(request.getParameter("firstName"));
+           student.setLastName(request.getParameter("lastName"));
+           student.setEmail(request.getParameter("email"));
+           student.setAddress(request.getParameter("address"));
+           student.setPassword(request.getParameter("password"));
+           
+           student.RegisterUser();
+           
+           out.println("<br>");
+           out.println("<br>");
+           out.println("<center> GREAT !!! </center>");
+           RequestDispatcher rd = request.getRequestDispatcher("Login.jsp");
+           rd.forward(request, response);
+       }finally {out.close();}
+           
+           
+           
+         
         }
-    }
-
+    
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
