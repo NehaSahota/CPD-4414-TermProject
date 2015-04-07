bf2378f0ec48247306ef26de2c9917f7f628e0d9
Neha <neha.sahota@gmail.com>
2015-04-06 22:27:00 -0400

    added get results , do update and doGet methods

--- src/java/credentials/feedback.java
+++ src/java/credentials/feedback.java
@@ -6,7 +6,18 @@
 
 package credentials;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 /**
  *
@@ -14,7 +25,61 @@ import javax.ws.rs.Path;
  */
 @Path("/feedback")
 public class feedback {
+    @GET
+    @Produces("application/json")
+    public String doGet() {
+        String str = getResults("SELECT * FROM feedback");
+        return str;
+    }
     
+    private String getResults(String query, String... params) {
+        JsonArrayBuilder productArray = Json.createArrayBuilder();
+        String myString = new String();
+        int inside=0;
+        try (Connection conn = Credentials.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+                inside=1;
+            }
+
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+
+                JsonObjectBuilder jsonob = Json.createObjectBuilder()
+                        .add("feedback_id", rs.getInt("id"))
+                        .add("id", rs.getString("id"))
+                        .add("feedback", rs.getString("feedback"))
+                        .add("category", rs.getString("category"));
+                       
+
+                myString = jsonob.build().toString();
+                productArray.add(jsonob);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(test.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if (inside == 0) {
+            myString = productArray.build().toString();
+        }
+        return myString;
+    }
+       
+       private int doUpdate(String query, String... params) {
+        int numChanges = 0;
+        try (Connection conn = Credentials.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+            numChanges = pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(test.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return numChanges;
+    }
     
     
 }
