0497a4251c57146cfc95c464a60d680e8ef3214b
Neha <neha.sahota@gmail.com>
2015-04-06 22:22:47 -0400

    feedback class added

---
+++ src/java/credentials/feedback.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package credentials;
+
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author c0646567
+ */
+@Path("/feedback")
+public class feedback {
+    
+    
+    
+}
